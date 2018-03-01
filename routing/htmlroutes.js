82240fcf5ec67bd25aee1e50e047f490bbd61835
Jared Henderson <jaredmhenderson@icloud.com>
2018-02-28 18:03:32 -0700

    changed file structure

---
+++ routing/htmlroutes.js
@@ -0,0 +1,20 @@
+//Dependancies
+
+const path = require('path');
+
+//Routing
+
+module.exports = function(app) {
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+    app.get('/survey', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+
+    app.get('/home', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+
+    
+};
